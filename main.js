9c32a3c3276d16ec147f4d01b6902568890ff484
Gwendal <gwendal.vasseur29800@gmail.com>
2023-06-10 21:10:43 +0200

    cc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//constante 
+//constante
 
 const cursor = document.getElementById("pointor");
 const footer = document.querySelector("footer");
@@ -38,9 +38,53 @@ window.addEventListener("resize", () => {
   }
 });
 
-projet.addEventListener("click", ()=>{
+// Fenetre modale
 
-  document.body.style.color ="red";
+const modalTrigger1 = document.querySelector(".modal-trigger1");
+const modal1 = document.getElementById("modal-1");
 
+const modalTrigger2 = document.querySelector(".modal-trigger2");
+const modal2 = document.getElementById("modal-2");
+
+const modalTrigger3 = document.querySelector(".modal-trigger3");
+const modal3 = document.getElementById("modal-3");
+
+const modalTrigger4 = document.querySelector(".modal-trigger4");
+const modal4 = document.getElementById("modal-4");
+
+const modalTrigger5 = document.querySelector(".modal-trigger5");
+const modal5 = document.getElementById("modal-5");
+
+const modalTrigger6 = document.querySelector(".modal-trigger6");
+const modal6 = document.getElementById("modal-6");
+
+const buttonModal = document.querySelectorAll(".button-modal");
+const modalContainer = document.querySelectorAll(".modal-container");
+
+modalTrigger1.addEventListener("click", () => {
+  modal1.classList.add("modal-container-toggle");
 });
+modalTrigger2.addEventListener("click", () =>
+  modal2.classList.add("modal-container-toggle")
+);
 
+modalTrigger3.addEventListener("click", () =>
+  modal3.classList.add("modal-container-toggle")
+);
+modalTrigger4.addEventListener("click", () =>
+  modal4.classList.add("modal-container-toggle")
+);
+modalTrigger5.addEventListener("click", () =>
+  modal5.classList.add("modal-container-toggle")
+);
+modalTrigger6.addEventListener("click", () =>
+  modal6.classList.add("modal-container-toggle")
+);
+
+buttonModal.forEach((element) =>
+  element.addEventListener("click", () => {
+    modalContainer.forEach((element) =>
+      element.classList.remove("modal-container-toggle")
+    );
+  })
+);
