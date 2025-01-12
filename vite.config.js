f0c7b0ab66c930ad8a436e85fb3d6afb8e8da49e
Gwendal Vasseur <gwendal.vasseur29800@gmail.com>
2025-01-12 13:26:10 +0100

    t

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ export default defineConfig({
     define: {
         // By default, Vite doesn't include shims for NodeJS/
         // necessary for segment analytics lib to work
-        global: {}
-      }
+        global: {},
+        __DEFINES__: JSON.stringify({}),
+      },
+      build: {
+        minify: 'esbuild', // Utilisez esbuild pour minimiser les erreurs
+      },
 });
