bdec4badf07db3c07af031181d157b6144a2bdce
Gwendal Vasseur <gwendal.vasseur29800@gmail.com>
2025-01-12 13:50:44 +0100

    t

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-   
+    define: {
+        // Définir une valeur par défaut pour éviter l'erreur
+        __DEFINES__: JSON.stringify({}),
+      },
     build:
     {
         outDir: '../dist', // Output in the dist/ folder
