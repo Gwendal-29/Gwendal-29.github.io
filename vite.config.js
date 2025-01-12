b1e466942dd2fb01a21bf58f556c16dc3319c539
Gwendal Vasseur <gwendal.vasseur29800@gmail.com>
2025-01-12 13:59:16 +0100

    jespere ça marcje

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    plugins: [],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Polyfills Node.js globals for browser compatibility
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
     define: {
         // Définir une valeur par défaut pour éviter l'erreur
         __DEFINES__: JSON.stringify({}),
@@ -13,4 +29,9 @@ export default defineConfig({
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
+    server: {
+        hmr: {
+          overlay: false, // Désactive l'affichage des erreurs HMR dans l'interface
+        },
+      },
 });
